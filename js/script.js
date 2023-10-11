0fb6b103b988b0d502b6e6ab180a5e1c16bcd135
muhzarfan <zarfan.alf@gmail.com>
2023-10-11 20:37:36 +0700

    add js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const title = document.getElementById('header-title')
+const conversionButton = document.getElementById('input-button')
+
+// conversionButton.addEventListener('click', function(){
+//     title.innerHTML = "wassaf"
+// })
+
+conversionButton.addEventListener('click', changeTitle)
+
+function changeTitle(){
+    title.innerHTML = "hello"
+}
+
+const integerRegex = /^-?\d+$/
+console.log(integerRegex.test("asade"))
+console.log(integerRegex.test("123"))
+console.log(integerRegex.test(456))
+console.log(integerRegex.test("1asf"))
